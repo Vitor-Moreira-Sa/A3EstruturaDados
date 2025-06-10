a7911e2392bf76020e11d5a07e7778a8cd7387d7
Vitor Moreira <vm040813@gmail.com>
2025-06-10 00:33:06 -0300

    Realiza novas modificações nos arquivos após renomeação das pastas

--- TrabalhoEstruturaDeDados/src/main/java/com/mycompany/questao2/Documento.java
+++ TrabalhoEstruturaDeDados/src/main/java/com/mycompany/questao2/Documento.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.a3trabalho;
+package com.mycompany.Questao2;
 
 import java.util.Date;
 
